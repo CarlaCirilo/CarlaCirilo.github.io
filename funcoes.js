351dc3f84458e88367333d6894e483271568995a
CarlaCirilo <37544734+CarlaCirilo@users.noreply.github.com>
2020-05-01 16:44:44 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,20 @@
+function funcaoQuemSomos() 
+{
+        alert('Os Adoradores da Divina Misericórdia são a expressão desse amor misericordioso de Deus, representados pela cor amarela. ')
+        
+    }
+    function funcaoMMAE() {
+        alert('A primeira missão é promover uma vigília noturna mensal, a realizar-se toda 1ª Sexta-feira de cada mês, em honra ao Sagrado Coração de Jesus e do Imaculado Coração de Maria. Desejamos através desta primeira iniciativa, reavivar um novo espírito de adoração eucarística, que possa contagiar outras paróquias, grupos e cidades por uma nova busca de vida de oração.  ')
+    }
+    function funcaoAdoracaoPerpetua(){
+alert('AdoracaoPerpetua')
+    }
+    function funcaoIntercessao(){
+alert('Intercessao')
+    }
+    function funcaoAtendimentoOracao(){
+alert('atendimentoOracao')
+    }
+    function funcaoMilAveMarias(){
+        alert('MilAveMarias ')
+    }
